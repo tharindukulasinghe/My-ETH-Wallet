d8994e697787560960813d0c816d2a08aaa089e4
rdtharindudk <dilshantharinduk@gmail.com>
2018-12-24 18:31:47 +0530

    implementing admob

--- screens/home.js
+++ screens/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { View, StatusBar } from "react-native";
+import { View, StatusBar, TouchableOpacity } from "react-native";
 import { CardItem, Text, Button, Left, Right } from "native-base";
+import { AdMobBanner, AdMobRewarded } from "react-native-admob";
 
 class HomeScreen extends Component {
   state = {};
@@ -9,44 +10,79 @@ class HomeScreen extends Component {
     title: "My ETH Wallet"
   };
 
+  componentDidMount() {
+    AdMobRewarded.setAdUnitID("ca-app-pub-5256999799292373~1338008968");
+    AdMobRewarded.requestAd().then();
+  }
+
   render() {
     return (
       <View
         style={{
-          margin: 5,
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "white"
+          flex: 1
         }}
       >
-        <StatusBar backgroundColor="#004D40" />
-        <CardItem>
-          <Left />
-          <Button
-            style={{ margin: 10 }}
-            success
-            large
-            bordered
-            onPress={() => this.goToAccounts()}
-          >
-            <Text> My Wallets </Text>
-          </Button>
-          <Right />
-        </CardItem>
-        <CardItem>
-          <Left />
-          <Button
-            style={{ margin: 10 }}
-            info
-            large
-            bordered
-            onPress={() => this.goToTopTokens()}
-          >
-            <Text> Top Tokens </Text>
-          </Button>
-          <Right />
-        </CardItem>
+        <View
+          style={{
+            margin: 10,
+            flex: 1,
+            justifyContent: "space-between",
+            alignItems: "stretch"
+          }}
+        >
+          <StatusBar backgroundColor="#004D40" />
+          <TouchableOpacity onPress={() => this.goToAccounts()}>
+            <CardItem style={{ backgroundColor: "white", borderRadius: 10 }}>
+              <Left />
+              <Button
+                style={{ margin: 30 }}
+                transparent
+                large
+                onPress={() => this.goToAccounts()}
+              >
+                <Text> My Wallets </Text>
+              </Button>
+              <Right />
+            </CardItem>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.goToTopTokens()}>
+            <CardItem style={{ backgroundColor: "white", borderRadius: 10 }}>
+              <Left />
+              <Button
+                style={{ margin: 30 }}
+                transparent
+                large
+                success
+                onPress={() => this.goToTopTokens()}
+              >
+                <Text> Top Tokens </Text>
+              </Button>
+              <Right />
+            </CardItem>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.showVideoAd()}>
+            <CardItem style={{ backgroundColor: "white", borderRadius: 10 }}>
+              <Left />
+              <Button
+                style={{ margin: 30 }}
+                transparent
+                large
+                info
+                onPress={() => this.showVideoAd()}
+              >
+                <Text> Coin Faucet </Text>
+              </Button>
+              <Right />
+            </CardItem>
+          </TouchableOpacity>
+        </View>
+
+        <AdMobBanner
+          adSize="smartBannerPortrait"
+          adUnitID="ca-app-pub-5256999799292373/1601022408"
+          testDevices={[AdMobBanner.simulatorId]}
+          onAdFailedToLoad={error => alert(error)}
+        />
       </View>
     );
   }
@@ -58,6 +94,38 @@ class HomeScreen extends Component {
   goToTopTokens() {
     this.props.navigation.navigate("TopTokens");
   }
+
+  showVideoAd() {
+    AdMobRewarded.requestAd().then(AdMobRewarded.showAd());
+    alert("No coin faucets available!");
+  }
 }
 
 export default HomeScreen;
+
+/* <CardItem>
+<Left />
+<Button
+  style={{ margin: 10 }}
+  success
+  large
+  bordered
+  onPress={() => this.goToAccounts()}
+>
+  <Text> My Wallets </Text>
+</Button>
+<Right />
+</CardItem>
+<CardItem>
+<Left />
+<Button
+  style={{ margin: 10 }}
+  info
+  large
+  bordered
+  onPress={() => this.goToTopTokens()}
+>
+  <Text> Top Tokens </Text>
+</Button>
+<Right />
+</CardItem> */
